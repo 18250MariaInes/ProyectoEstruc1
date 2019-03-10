0997b56bb9c0f04a69bd7240b9382de00e04ced5
Camila <gon18398@uvg.edu.gt>
2019-03-09 21:25:01 -0600

    El main puede leer txt linea por linea y separarla adecuadamente

--- Proyecto1/src/MainLisp.java
+++ Proyecto1/src/MainLisp.java
@@ -11,15 +11,19 @@
 import java.util.*;
  import java.text.*;
  import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 public class MainLisp {
     public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     String comando;
     Calculadora calculadora=new Calculadora();
-    HashMap<String, ArrayList<String>> instrucciones = new HashMap<String, ArrayList<String>>();
+    HashMap<String, ArrayList<String>> instrucciones = new HashMap<>();
     /* String to Split
     System.out.println("Introduzca comando en Lisp que desee ejecutar");
     comando = scan.nextLine();
@@ -31,12 +35,31 @@ public class MainLisp {
     // Se imprime el resultado obtenido al operar los elementos de la nueva lista
     int resultado=calculadora.resultadodeoperacion(itemList);
     System.out.println("El resultado de tu operacion es: " + resultado);*/
+    //Se lee el .txt
+    ArrayList<String> lectura = new ArrayList<>();
     Analizador separador = new Analizador();
+    try{
+        Stream<String> lines = Files.lines(
+            Paths.get("comands.txt"),
+            StandardCharsets.UTF_8
+        );
+        lines.forEach(c -> lectura.add(c));
+        for (int i=0; i<=(lectura.size()-1); i++){
+            //Se itera la lectura y cada elemento se divide en dos (nombre-tipo)
+            String item = lectura.get(i);
+            instrucciones = separador.separar(item);
+            System.out.print("\nHASH DEL MAIN ----> " + instrucciones);
+            System.out.print("\n\n");
+        }
+    } catch (IOException e){
+            System.out.println("Error al leer el archivo");
+    }
+    /*Analizador separador = new Analizador();
     instrucciones = separador.separar("(LIST 1 2 3 4 5(+ 8 9)))");
     System.out.print("\nHASH DEL MAIN ----> " + instrucciones);
     System.out.print("\n\n");
     instrucciones = separador.separar("(DEFUN queTanBienLeCaigoADiego (nombre) 0)");
-    System.out.print("\nHASH DEL MAIN ----> " +instrucciones);
+    System.out.print("\nHASH DEL MAIN ----> " +instrucciones);*/
     }
     
     
